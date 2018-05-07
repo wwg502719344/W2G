f3f446f4ae423d2e31b3d853489d5760daab2f7c
502719344 <502719344@qq.com>
2018-05-07 15:57:08 +0800

    提交代码至github

---
+++ src/cource_code/AQS.java
@@ -0,0 +1,48 @@
+package cource_code;
+
+/**
+ * Created by W2G on 2018/3/29.
+ */
+public class AQS {
+
+    /**
+     * 共享式获取同步状态
+     */
+    /*
+    //尝试获取同步状态
+    public final void acquireShared(int arg) {
+        if (tryAcquireShared(arg) < 0)
+            doAcquireShared(arg);
+    }
+
+    //获取共享状态失败，进入等待队列，获取资源才返回
+    private void doAcquireShared(int arg) {
+        //构造共享节点加入队列
+        final Node node = addWaiter(Node.SHARED);
+        boolean failed = true;
+        try {
+            boolean interrupted = false;
+            for (;;) {
+                final Node p = node.predecessor();
+                if (p == head) {
+                    int r = tryAcquireShared(arg);
+                    if (r >= 0) {
+                        setHeadAndPropagate(node, r);
+                        p.next = null; // help GC
+                        if (interrupted)
+                            selfInterrupt();
+                        failed = false;
+                        return;
+                    }
+                }
+                if (shouldParkAfterFailedAcquire(p, node) &&
+                    parkAndCheckInterrupt())
+                    interrupted = true;
+            }
+        } finally {
+            if (failed)
+                cancelAcquire(node);
+        }
+    }
+    */
+}
