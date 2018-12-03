ed10a8dae0963b13093a42deef0d45b9edcfd322
502719344 <502719344@qq.com>
2018-12-03 11:07:15 +0800

    12-3:HashMap源码解析

--- src/w2g_ConcurrentHashMap/hashmap_detail/HashMap_Source.java
+++ src/w2g_ConcurrentHashMap/hashmap_detail/HashMap_Source.java
@@ -9,7 +9,53 @@ public class HashMap_Source {
 
     HashMap map=new HashMap();
 
-    public void main(String args[]){
-        map.put("1","2");
-    }
+    /**
+     *
+     */
+    /*
+    final V putVal(int hash, K key, V value, boolean onlyIfAbsent,
+                   boolean evict) {
+        Node<K,V>[] tab; Node<K,V> p; int n, i;
+        //步骤1:查看当前数组是否为空(长度是否为0)，如果判断条件为true，则对tab进行初始化，并对n进行赋值
+        if ((tab = table) == null || (n = tab.length) == 0)
+            n = (tab = resize()).length;
+        //步骤2:如果在tab位置的节点为null，则初始化一个
+        if ((p = tab[i = (n - 1) & hash]) == null)
+            tab[i] = newNode(hash, key, value, null);
+        //步骤3:数组该位置有数据
+        else {
+            Node<K,V> e; K k;
+            if (p.hash == hash &&
+                    ((k = p.key) == key || (key != null && key.equals(k))))
+                e = p;
+            else if (p instanceof TreeNode)
+                e = ((TreeNode<K,V>)p).putTreeVal(this, tab, hash, key, value);
+            else {
+                for (int binCount = 0; ; ++binCount) {
+                    if ((e = p.next) == null) {
+                        p.next = newNode(hash, key, value, null);
+                        if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
+                            treeifyBin(tab, hash);
+                        break;
+                    }
+                    if (e.hash == hash &&
+                            ((k = e.key) == key || (key != null && key.equals(k))))
+                        break;
+                    p = e;
+                }
+            }
+            if (e != null) { // existing mapping for key
+                V oldValue = e.value;
+                if (!onlyIfAbsent || oldValue == null)
+                    e.value = value;
+                afterNodeAccess(e);
+                return oldValue;
+            }
+        }
+        ++modCount;
+        if (++size > threshold)
+            resize();
+        afterNodeInsertion(evict);
+        return null;
+    }*/
 }
