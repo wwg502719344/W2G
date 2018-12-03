b94f22634335c386f58695d6c722b53bab995f57
502719344 <502719344@qq.com>
2018-12-03 19:34:26 +0800

    12-3:HashMap源码解析

--- src/w2g_ConcurrentHashMap/hashmap_detail/HashMap_Source.java
+++ src/w2g_ConcurrentHashMap/hashmap_detail/HashMap_Source.java
@@ -25,29 +25,36 @@ public class HashMap_Source {
         //步骤3:数组该位置有数据
         else {
             Node<K,V> e; K k;
+            //步骤3-1:如果在hashmap中发现该节点，则
             if (p.hash == hash &&
                     ((k = p.key) == key || (key != null && key.equals(k))))
                 e = p;
+            //步骤3-2:如果该节点是红黑树结构的节点，则通过红黑树的方式将该节点插入
             else if (p instanceof TreeNode)
                 e = ((TreeNode<K,V>)p).putTreeVal(this, tab, hash, key, value);
             else {
+                //步骤3-3:此处说明数据是链表结构
                 for (int binCount = 0; ; ++binCount) {
+                    //如果该链表节点没有传入的节点则创建一个节点添加到链表的尾部
+                    //如果链表长度大于指定长度，则转为红黑树结构
                     if ((e = p.next) == null) {
                         p.next = newNode(hash, key, value, null);
                         if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
                             treeifyBin(tab, hash);
                         break;
                     }
+                    //链表中已经有相等的key，则进行值覆盖
                     if (e.hash == hash &&
                             ((k = e.key) == key || (key != null && key.equals(k))))
                         break;
                     p = e;
                 }
             }
+            //此处的e代表的是具有相同key的老节点,由步骤3-1/2可知
             if (e != null) { // existing mapping for key
                 V oldValue = e.value;
                 if (!onlyIfAbsent || oldValue == null)
-                    e.value = value;
+                    e.value = value;//对e值进行覆盖
                 afterNodeAccess(e);
                 return oldValue;
             }
