8b8ad2afc1ec5dea10175444014be3fbb2b857c6
wwg <502719344@qq.cmo>
2019-06-07 15:09:46 +0800

    5-14:原子操作解析

--- src/w2g_concurrent_utils/countDownLatch_source.java
+++ src/w2g_concurrent_utils/countDownLatch_source.java
@@ -1,5 +1,7 @@
 package w2g_concurrent_utils;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Created by W2G on 2018/8/26 0026.
  * 允许一个或多个线程等待其他线程完成操作
@@ -7,6 +9,51 @@ package w2g_concurrent_utils;
  *
  */
 public class countDownLatch_source {
+
+
+    CountDownLatch c=new CountDownLatch(2);
+
+    /**
+     * P0
+     * CountDownLatch的两种基本用法
+     * demo来自java api
+     * Driver类中标记了startSignal和doneSignal，只有当driver类执行到指定位置
+     * 时，才可以执行线程worker类，否则阻塞，只有worker类执行结束后，才可以执行
+     * Driver后续类
+     * 有点像游戏里面到控制技能
+     */
+    /*class Driver { // ...
+       void main() throws InterruptedException {
+         CountDownLatch startSignal = new CountDownLatch(1);
+         CountDownLatch doneSignal = new CountDownLatch(N);
+
+         for (int i = 0; i < N; ++i) // create and start threads
+           new Thread(new Worker(startSignal, doneSignal)).start();
+
+         doSomethingElse();            // don't let run yet
+         startSignal.countDown();      // let all threads proceed
+         doSomethingElse();
+         doneSignal.await();           // wait for all to finish
+       }
+     }
+
+     class Worker implements Runnable {
+       private final CountDownLatch startSignal;
+       private final CountDownLatch doneSignal;
+       Worker(CountDownLatch startSignal, CountDownLatch doneSignal) {
+          this.startSignal = startSignal;
+          this.doneSignal = doneSignal;
+       }
+       public void run() {
+          try {
+            startSignal.await();
+            doWork();
+            doneSignal.countDown();
+    } catch (InterruptedException ex) {} // return;
+       }
+
+       void doWork() { ... }
+     }*/
 
     /**
      * P1
